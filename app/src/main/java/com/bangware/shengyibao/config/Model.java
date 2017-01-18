5c0f26295e721fe568b4a2493354e3341d8ae2cc
qiubo <352959518@qq.com>
2017-01-18 11:05:35 +0800

    库存代码

--- app/src/main/java/com/bangware/shengyibao/config/Model.java
+++ app/src/main/java/com/bangware/shengyibao/config/Model.java
@@ -15,10 +15,11 @@ public class Model {
 		//登录
 		public static String LOGINURL = "users/sign_in.json";
 
-	public static String QUERY_CAR=HTTPURL+"mobile_interfaces/mobile_info?method=get_ladingbill_car_number";
+        //提货/开单车牌号查询
+	    public static String QUERY_CAR=HTTPURL+"mobile_interfaces/mobile_info?method=get_ladingbill_car_number";
 
 	     //修改密码
-	   public  static String UPDATEPASSWORD="users.json";
+	    public  static String UPDATEPASSWORD="users.json";
 		
 		//app业务员版本升级接口
 		public static String UPDATEVERSION = HTTPURL+"mobile_interfaces/mobile_info?method=update_app_version";
